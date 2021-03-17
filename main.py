2dc2c8f0df35efcb08fb3f091515647f3a623b35
Jittat Fakcharoenphol <jittat@gmail.com>
2021-03-17 15:18:10 +0700

    Pacmans movement, eat dots, move freely

--- main.py
+++ main.py
@@ -9,6 +9,50 @@ CANVAS_HEIGHT = 600
 
 UPDATE_DELAY = 33
 
+DIR_STILL = 0
+DIR_CONTINUE = -1
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_LEFT = 4
+
+DIR_OFFSET = {
+    DIR_STILL: (0,0),
+    DIR_UP: (0,-1),
+    DIR_RIGHT: (1, 0),
+    DIR_DOWN: (0, 1),
+    DIR_LEFT: (-1, 0),
+}
+
+PACMAN_SPEED = 5
+
+class Pacman(Sprite):
+    def __init__(self, app, maze, r, c):
+        self.r = r
+        self.c = c
+        self.maze = maze
+
+        self.direction = DIR_STILL
+        self.next_direction = DIR_STILL
+
+        x, y = maze.piece_center(r,c)
+        super().__init__(app, 'images/pacman.png', x, y)
+
+    def update(self):
+        if self.maze.is_at_center(self.x, self.y):
+            r, c = self.maze.xy_to_rc(self.x, self.y)
+
+            if self.maze.has_dot_at(r, c):
+                self.maze.eat_dot_at(r, c)
+            
+            self.direction = self.next_direction
+
+        self.x += PACMAN_SPEED * DIR_OFFSET[self.direction][0]
+        self.y += PACMAN_SPEED * DIR_OFFSET[self.direction][1]
+
+    def set_next_direction(self, direction):
+        self.next_direction = direction
+
 
 class PacmanGame(GameApp):
     def create_sprites(self):
@@ -19,6 +63,12 @@ class PacmanGame(GameApp):
 
         self.maze = Maze(self, CANVAS_WIDTH, CANVAS_HEIGHT)
 
+        self.pacman1 = Pacman(self, self.maze, 1, 1)
+        self.pacman2 = Pacman(self, self.maze, self.maze.get_height() - 2, self.maze.get_width() - 2)
+
+        self.elements.append(self.pacman1)
+        self.elements.append(self.pacman2)
+
     def pre_update(self):
         pass
 
@@ -26,8 +76,23 @@ class PacmanGame(GameApp):
         pass
 
     def on_key_pressed(self, event):
-        pass
+        if event.char.upper() == 'A':
+            self.pacman1.set_next_direction(DIR_LEFT)
+        elif event.char.upper() == 'W':
+            self.pacman1.set_next_direction(DIR_UP)
+        elif event.char.upper() == 'S':
+            self.pacman1.set_next_direction(DIR_DOWN)
+        elif event.char.upper() == 'D':
+            self.pacman1.set_next_direction(DIR_RIGHT)
 
+        if event.char.upper() == 'J':
+            self.pacman2.set_next_direction(DIR_LEFT)
+        elif event.char.upper() == 'I':
+            self.pacman2.set_next_direction(DIR_UP)
+        elif event.char.upper() == 'K':
+            self.pacman2.set_next_direction(DIR_DOWN)
+        elif event.char.upper() == 'L':
+            self.pacman2.set_next_direction(DIR_RIGHT)
 
 if __name__ == "__main__":
     root = tk.Tk()
--- maze.py
+++ maze.py
@@ -4,6 +4,12 @@ class Dot(Sprite):
     def __init__(self, app, x, y):
         super().__init__(app, 'images/dot.png', x, y)
 
+        self.is_eaten = False
+
+    def get_eaten(self):
+        self.is_eaten = True
+        self.hide()
+
 
 class Wall(Sprite):
     def __init__(self, app, x, y):
@@ -27,12 +33,29 @@ class Maze:
         "####################",    
     ]
 
+    WALL_CHAR = '#'
+    DOT_CHAR = '.'
+
     def piece_center(self, r, c):
-        return (c*40 + 20, self.canvas_height - (r * 40) - 60)
+        return (c*40 + 20, 60 + (r * 40))
+
+    def is_at_center(self, x, y):
+        return ((x - 20) % 40 == 0) and ((y - 60) % 40 == 0)
+
+    def xy_to_rc(self, x, y):
+        return ((y - 60) // 40, (x - 20) // 40)
+
+    def init_active_dots(self):
+        self.has_active_dots = {}
+        for i in range(self.get_height()):
+            for j in range(self.get_width()):
+                self.has_active_dots[(i,j)] = Maze.MAP[i][j] == Maze.DOT_CHAR
 
     def init_maze_sprites(self):
-        self.dots = []
         self.walls = []
+        self.dots = {}
+
+        self.init_active_dots()
 
         for i in range(self.get_height()):
             for j in range(self.get_width()):
@@ -44,7 +67,7 @@ class Maze:
 
                 if self.has_dot_at(i, j):
                     dot = Dot(self.app, x, y)
-                    self.dots.append(dot)            
+                    self.dots[(i,j)] = dot
 
     def __init__(self, app, canvas_width, canvas_height):
         self.app = app
@@ -63,10 +86,19 @@ class Maze:
         pass
 
     def has_wall_at(self, r, c):
-        return Maze.MAP[r][c] == '#'
+        return Maze.MAP[r][c] == Maze.WALL_CHAR
 
     def has_dot_at(self, r, c):
-        return Maze.MAP[r][c] == '.'
+        if (r,c) in self.has_active_dots:
+            return self.has_active_dots[(r,c)]
+        else:
+            return False
+
+    def eat_dot_at(self, r, c):
+        dot = self.dots[(r,c)]
+        dot.get_eaten()
+
+        self.has_active_dots[(r,c)] = False
 
     def get_height(self):
         return len(Maze.MAP)
